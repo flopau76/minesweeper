3d6c251b4165ebee555c36b787c699a9d08b86e0
MrBigoudi <yannis.kedadry2@gmail.com>
2024-03-25 21:14:52 +0100

    trying to fix mlp

--- MLP.py
+++ MLP.py
@@ -35,21 +35,22 @@ NN_L2 = 128 # neurons in the second layer
 NN_L3 = 128 # neurons in the third layer
 NN_L4 = 128 # neurons in the fourth layer
 
-MIN_PROBA = 1e-3 # probability under which select a random cell instead of border cell
+MIN_PROBA = 0.05 # probability under which select a random cell instead of border cell
 
+GUESS_REWARD = -1 # randomly guessed the next play
 CONTINUE_REWARD = 1 # the reward gained each turn
 LOSS_REWARD = -50   # the reward if we lose the game
-WIN_REWARD = 50     # the reward if we won the game
+WIN_REWARD = 50    # the reward if we won the game
 
-EPSILON_START = 0.82   # the initial epsilon value
-EPSILON_MIN = 0.013    # the minimum epsilon value
-EPSILON_DECAY = 0.9675 # the decay epsilon value
+EPSILON_START = 0.9   # the initial epsilon value
+EPSILON_MIN = 0.05    # the minimum epsilon value
+EPSILON_DECAY = 0.999 # the decay epsilon value
 
-NUM_EPISODES = 150 # the number of episode per training
+NUM_EPISODES = 1000000 # the number of episode per training
 GAMMA = 0.9 # the gamma value for QLearning
 
-NUM_TRAININGS = 20 # the number of trainings
-LR = 0.01 # the learning rate
+NUM_TRAININGS = 1 # the number of trainings
+LR = 0.001 # the learning rate
 # ------------------
 
 class MLPModel(nn.Module):
@@ -92,7 +93,7 @@ class MLPModel(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, x):
+    def forward(self, x, mask):
         '''Forward pass for the Neural Network'''
         batch_size = x.size(0)
         x = self.layer1(x)
@@ -100,6 +101,7 @@ class MLPModel(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.layer5(x)
+        x = x*mask
         return x.squeeze(0)
 
 
@@ -114,11 +116,12 @@ class EpsilonGreedy():
         epsilon_min: The minimum epsilon value
         epsilon_decay: The epsilon decay value
     '''
-    def __init__(self, model, epsilon_start=EPSILON_START, epsilon_min=EPSILON_MIN, epsilon_decay=EPSILON_DECAY):
+    def __init__(self, model, device, epsilon_start=EPSILON_START, epsilon_min=EPSILON_MIN, epsilon_decay=EPSILON_DECAY):
         self.epsilon = epsilon_start
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
         self.model = model
+        self.device = device
 
     def get_random_cell(self, game):
         ''' Chose a random cell to uncover'''
@@ -126,23 +129,26 @@ class EpsilonGreedy():
         a = random.choice(A)
         return a
 
-    def __call__(self, observable_cells_ids, state, game):
+    def __call__(self, border_mask, state, game):
         '''Caller for the epsilon greedy algorithm'''
-        val = np.random.random_sample()
+        val = np.random.rand()
         if(val < self.epsilon or state.size == 0): # if first step return random action
             action = self.get_random_cell(game)
-            return action
+            return action, True
 
-        state_tensor = torch.tensor(state, dtype=torch.float32, device=device)
-        values = self.model(state_tensor)
+        state_tensor = torch.tensor(state, dtype=torch.float32, device=self.device)
+        values = self.model(x=state_tensor, mask=border_mask)
         if torch.max(values) < MIN_PROBA:
             action = self.get_random_cell(game)
-            return action
-        index = torch.argmin(values)
+            return action, True
+        index = torch.argmax(values)
         # print("values: ", values)
         # print("index: ", index)
-        action = observable_cells_ids[index.item()]
-        return action
+        # print("state: ", state)
+        # print("mask: ", border_mask)
+        action = env.int2tupple(index.item(), game.n_cols)
+        # print("action: ", action)
+        return action, False
 
     def decay_epsilon(self):
         '''Update epsilon's value'''
@@ -169,16 +175,19 @@ class NeuralNetworkStrategy():
             game = env.Minesweeper("beginner", display=False) # train on beginner mode
         self.env = game
 
-    def get_bordering_cells(self):
-        '''Get the bordering cells of uncovered cells'''
-        border = set()
+    def get_bordering_cells(self, device):
+        '''Get a mask for the bordering cells of uncovered cells'''
+        border = np.zeros(self.env.O.shape)
         uncovered = self.env.get_covered_cells(covered=False)
         for cell in uncovered:
             neigbhors = self.env.get_neighbors(cell)
             for (i,j) in neigbhors:
                 if self.env.O[i,j] == -2: # covered tile
-                    border.add((i,j))
-        return list(border)
+                    border[i,j] = 1
+        
+        border = torch.tensor(border, dtype=torch.float32, device=device)
+        border = border.view(-1).unsqueeze(1)
+        return border
 
     def get_5x5_neighborhood(self, cell):
         '''Get a list of exactly 25 neighbors id (put a tuple (-1,-1) if it is not a valid cell)'''
@@ -217,10 +226,11 @@ class NeuralNetworkStrategy():
         return 
             the observations of the cells (or -5 if the cell doesn't exist)
         '''
-        arr = np.array([self.env.O[i,j] if (not i==-1 and not j==-1) else -5 for (i,j) in cell_list])
+        # divide by 8 to be in [-1, 1]
+        arr = np.array([self.env.O[i,j] / 8 if (not i==-1 and not j==-1) else -5 for (i,j) in cell_list])
         return arr
 
-    def get_state(self):
+    def get_state(self, device):
         '''
         get the current state of the environment
 
@@ -228,14 +238,17 @@ class NeuralNetworkStrategy():
             bordering_cells: the cells we want to uncover
             state: the features to represent these cells
         '''
-        bordering_cells = self.get_bordering_cells()
-        state = np.array([self.convert_cells_to_feature(self.get_5x5_neighborhood(cell)) for cell in bordering_cells])
+        bordering_cells = self.get_bordering_cells(device)
+        all_cells = [(i,j) for i in range(self.env.n_rows) for j in range(self.env.n_cols)]
+        state = np.array([self.convert_cells_to_feature(self.get_5x5_neighborhood(cell)) for cell in all_cells])
         return bordering_cells, state
 
-    def get_reward(self, over):
+    def get_reward(self, over, random):
         '''Get the reward given the `over' state'''
         reward = 0
-        if over == 0: # continue
+        if over == 0 and random: # random guess
+            reward = GUESS_REWARD
+        elif over == 0: # found good uncovered cell
             reward = CONTINUE_REWARD
         elif over == 1: # loss
             reward = LOSS_REWARD
@@ -243,29 +256,31 @@ class NeuralNetworkStrategy():
             reward = WIN_REWARD
         return reward
 
-    def train_model_episode(self, model, epsilon_greedy, optimizer, loss_fn, device, num_episodes=NUM_EPISODES, gamma=GAMMA):
+    def train_model_episode(self, model, epsilon_greedy, optimizer, loss_fn, device, num_episodes=NUM_EPISODES, gamma=GAMMA, print_every=500):
         '''One training'''
         episode_reward_list = []
+        episode_loss_list = []
+        mean_reward = 0
+        mean_loss = 0
 
-        for episode_index in tqdm(range(1, num_episodes)):
+        # for episode_index in tqdm(range(1, num_episodes)):
+        for episode_index in range(1, num_episodes+1):
             self.reset_env(game = None)
-            bordering_cells, state = self.get_state()
+            bordering_cells, state = self.get_state(device)
             episode_reward = 0
+            episode_loss = 0
+            time_episode = 0
+            loss = torch.tensor(0.0, device=device)
 
-            # print("state: ", state)
-
-            for t in itertools.count():
-                action = epsilon_greedy(bordering_cells, state, self.env)
-                # print("action: ", action)
-
-                over = self.env.take_action(action, uncover_neighbors=True)
-                # print("over: ", over)
-                
-                reward = self.get_reward(over)
-                # print("reward: ", reward)
+            over = 0
+            while(over == 0):
+                time_episode += 1
+                action, random_guess = epsilon_greedy(bordering_cells, state, self.env)
+                over = self.env.take_action(action, uncover_neighbors=False)
+                reward = self.get_reward(over, random_guess)
                 
                 episode_reward += reward
-                next_bordering_cells, next_state = self.get_state()
+                next_bordering_cells, next_state = self.get_state(device)
 
                 if(not state.size == 0): # not empty state
                     state_tensor = torch.tensor(state, dtype=torch.float32, device=device)
@@ -273,47 +288,54 @@ class NeuralNetworkStrategy():
                     reward_tensor = torch.tensor(reward, dtype=torch.float32, device=device)
 
                     with torch.no_grad():
-                        values_next = model(next_state_tensor)
+                        values_next = model(x=next_state_tensor, mask=next_bordering_cells)
                         max_value_next = torch.max(values_next)
                         target_value = reward_tensor + gamma * max_value_next
 
-                    values = model(state_tensor)
-                    # print("values: ", values)
+                    values = model(x=state_tensor, mask=bordering_cells)
                     predicted_value = torch.max(values)
-                    # print("predicted value: ", predicted_value)
-                    # print("target value: ", target_value)
 
                     loss = loss_fn(predicted_value, target_value)
+                    episode_loss += loss.item()
 
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
 
-                if not (over==0):
-                    break
+                    mean_loss += loss.item() / print_every
 
                 state = next_state
                 bordering_cells = next_bordering_cells
 
+            mean_reward += episode_reward / print_every
+
+            if episode_index % print_every == 0:
+                print(f"episode: {episode_index}, Mean reward: {mean_reward}, Mean loss: {mean_loss}, Epsilon: {epsilon_greedy.epsilon}" 
+                )
+                mean_reward = 0
+                mean_loss = 0
+
             episode_reward_list.append(episode_reward)
+            episode_loss_list.append(episode_loss / time_episode)
             epsilon_greedy.decay_epsilon()
 
-        return episode_reward_list
+        return episode_reward_list, episode_loss_list
 
 
 
     def train_model(self, device, save_file, number_of_trainings=NUM_TRAININGS, lr=LR):
         '''Completely train the model'''
-        trains_result_list = [[], [], []]
+        trains_result_list = [[], []] # [0] -> reward list, [1] -> loss list
+        model = 0
 
         for train_index in range(number_of_trainings):
             model = MLPModel(n_observations=24).to(device)
             optimizer = torch.optim.AdamW(model.parameters(), lr=lr, amsgrad=True)
             loss_fn = torch.nn.MSELoss()
 
-            epsilon_greedy = EpsilonGreedy(model=model)
+            epsilon_greedy = EpsilonGreedy(model=model, device=device)
 
-            episode_reward_list = self.train_model_episode(
+            episode_reward_list, episode_loss_list = self.train_model_episode(
                 model=model, 
                 epsilon_greedy=epsilon_greedy, 
                 optimizer=optimizer, 
@@ -321,20 +343,22 @@ class NeuralNetworkStrategy():
                 device=device
             )
 
-            trains_result_list[0].extend(range(len(episode_reward_list)))
-            trains_result_list[1].extend(episode_reward_list)
-            trains_result_list[2].extend([train_index for _ in episode_reward_list])
+            # trains_result_list[0].extend(range(len(episode_reward_list)))
+            trains_result_list[0].append(episode_reward_list)
+            trains_result_list[1].append(episode_loss_list)
+            # trains_result_list[3].extend([train_index for _ in episode_reward_list])
 
-        naive_trains_result_df = pd.DataFrame(np.array(trains_result_list).T, columns=["num_episodes", "mean_final_episode_reward", "training_index"])
-        naive_trains_result_df["agent"] = "Naive"
+        # naive_trains_result_df = pd.DataFrame(np.array(trains_result_list).T, columns=["num_episodes", "mean_final_episode_reward", "training_index"])
+        # naive_trains_result_df["agent"] = "Naive"
 
         # Save the action-value estimation function of the last train
         torch.save(model, save_file)
         self.model = model
+        return trains_result_list
 
     def uncover_cell(self, cell):
         ''' Uncover a cell'''
-        over = self.env.take_action(cell, uncover_neighbors=True)
+        over = self.env.take_action(cell, uncover_neighbors=False)
         if self.verbose >=2:
             self.env.update_display()
             plt.waitforbuttonpress()
@@ -345,31 +369,32 @@ class NeuralNetworkStrategy():
         ''' Solve the game'''
         over = False
         while not over:
-            bordering_cells, state = self.get_state()
+            bordering_cells, state = self.get_state(device)
 
             action = self.get_random_cell()
             if(state.size > 0):
                 state_tensor = torch.tensor(state, dtype=torch.float32, device=device)
-                values = self.model(state_tensor)
+                values = self.model(x=state_tensor, mask=bordering_cells)
                 max_value = torch.max(values)
-                if(max_value < 1e-3):
+                if(max_value < MIN_PROBA):
                     action = self.get_random_cell()
                 else:
-                    action = bordering_cells[torch.argmin(values)]
+                    index = torch.argmax(values)
+                    action = env.int2tupple(index.item(), self.env.n_cols)
 
             over = self.uncover_cell(action)
                 
-        self.env.show_mines()
-        self.env.print_env()
-        self.env.update_display()
-        if over == 1:
-            print("You lost")
-        elif over == 2:
-            print("You won")
+        # self.env.show_mines()
+        # self.env.print_env()
+        # self.env.update_display()
+        # if over == 1:
+        #     print("You lost")
+        # elif over == 2:
+        #     print("You won")
         return over
 
 
-def load_or_train_model(solver, device, number_of_trainings=NUM_TRAININGS, lr=LR, force_retrain=False):
+def load_or_train_model(solver, device, number_of_trainings=NUM_TRAININGS, lr=LR, force_retrain=False, verbose=0):
     '''Helper function to check if the model must be trained or loaded from a file'''
     model_file = "first_mlp.pth"
 
@@ -378,7 +403,33 @@ def load_or_train_model(solver, device, number_of_trainings=NUM_TRAININGS, lr=LR
         solver.model = torch.load(model_file, map_location=device)
     else:
         print("Training new model...")
-        solver.train_model(device=device, save_file=model_file, number_of_trainings=number_of_trainings, lr=lr)
+        trains_result_list = solver.train_model(device=device, save_file=model_file, number_of_trainings=number_of_trainings, lr=lr)
+
+        if verbose > 1:
+            # Plot losses and rewards for each training run
+            num_episodes = len(trains_result_list[0][0])  # Get the number of episodes from the first training
+            num_trainings = len(trains_result_list[0])
+
+            # Plot rewards
+            plt.figure(figsize=(12, 6))
+            for i in range(num_trainings):
+                plt.plot(range(num_episodes), trains_result_list[0][i], label=f"Training {i+1}")
+            plt.xlabel("Episode")
+            plt.ylabel("Reward")
+            plt.title("Rewards per Episode")
+            plt.legend()
+            plt.show()
+
+            # Plot losses
+            plt.figure(figsize=(12, 6))
+            for i in range(num_trainings):
+                plt.plot(range(num_episodes), trains_result_list[1][i], label=f"Training {i+1}")
+            plt.xlabel("Episode")
+            plt.ylabel("Loss")
+            plt.title("Losses per Episode")
+            plt.legend()
+            plt.show()
+
     return
 
 
@@ -392,7 +443,7 @@ if __name__ == "__main__":
     nb_retrainings = 1
     for _ in range(nb_retrainings):
         tic = time.perf_counter()
-        load_or_train_model(solver=solver, device=device, force_retrain=True)
+        load_or_train_model(solver=solver, device=device, force_retrain=True, verbose=0)
         toc = time.perf_counter()
         cur_time = toc - tic
         print(f"Trained the model in {toc - tic:0.4f} seconds")
@@ -408,5 +459,5 @@ if __name__ == "__main__":
         if over == 2:
             nb_wins += 1
     
-    print(f"Random strategy: {nb_wins / nb_tests} wins on average")
+    print(f"MLP strategy: {nb_wins / nb_tests} wins on average ({nb_wins} / {nb_tests})")
     print()
